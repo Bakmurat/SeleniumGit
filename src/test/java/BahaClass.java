6da4b855a2aa3b1be3f9cf02f04b1a8880a4626f
first_ch <bakmuratkubanaliev@gmail.com>
2022-07-09 15:00:22 -0400

    from master changes

--- src/test/java/BahaClass.java
+++ src/test/java/BahaClass.java
@@ -2,5 +2,6 @@ public class BahaClass {
     public static void main(String[] args) {
         System.out.println("Baha started his project");
         System.out.println("Baha finished first changes in his file");
+        System.out.println("from master changes");
     }
 }
