af6934ab3bffaedda943bb000da4fd876ea979b1
first_ch <bakmuratkubanaliev@gmail.com>
2022-07-09 14:17:09 -0400

    Third Change

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,5 +2,6 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Hello Test");
         System.out.println("Made more changes");
+        System.out.println("I like changes");
     }
 }
